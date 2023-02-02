ade8991e3836a6e6e8c24b9eb45875a56dde28dd
dominikmi <distantnights@gmail.com>
2023-02-02 22:33:06 +0100

    extend function

--- ghcommits.py
+++ ghcommits.py
@@ -6,26 +6,29 @@ from pathlib import Path
 
 # the function takes nested dictionary as argument.
 
-def print_commit_attr(commitjson):
+def print_commit_attr(*args):
     index = 0
-    dict_len = len(commitjson)
+    number_args=len(args)
+    dict_len = len(args[0])
     while (index < dict_len):
-        vals = commitjson[index]
+        vals = args[0][index]
         line = f'Date: {vals["commit"]["author"]["date"]}, Author: {vals["commit"]["author"]["name"]}, Message: {vals["commit"]["message"]}'
-        print(line)
+        if number_args == 1: 
+            print(line)
+        else:
+            while (Path(args[1]).is_dir() or Path(args[1]).exists()): 
+                break
+            else: 
+                Path(args[1]).touch()
+                Path(args[1]).write_text(line)
         index += 1
-    print("-" * len(line))
-    print(f"Number of commits in the repository: {args.reponame} : {dict_len}")
-
-# function which dumps output to filename passed in the argument
-
-def dumptofile(data, filename):
-    while (Path(filename).is_dir() or Path(filename).exists()): 
-        break
-    else: 
-        Path(filename).touch()
-        Path(filename).write_text(data)
-
+    if number_args == 1: 
+        print("-" * len(line))
+        print(f"Number of commits in the repository: {cliargs.reponame} : {dict_len}")
+    else:
+        Path(args[1]).write_text("-" * len(line))
+        lastline = f"Number of commits in the repository: {cliargs.reponame} : {dict_len}"
+        Path(args[1]).write_text(lastline)
 
 # parse arguments
 
@@ -36,17 +39,17 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-r", "--reponame", type=str, help="Repository Name")
 parser.add_argument("-o", "--ownername", type=str, help="Owner Name")
 parser.add_argument("-f", "--fileout", type=str, help="Output filename")
-args = parser.parse_args()
+cliargs = parser.parse_args()
 
 # Set headers and token
 
 token = os.getenv('GITHUB_API_TOKEN', '...')
-url = f"https://api.github.com/repos/{args.ownername}/{args.reponame}/commits"
+url = f"https://api.github.com/repos/{cliargs.ownername}/{cliargs.reponame}/commits"
 headers = {'Authorization': f'token {token}'}
 
 # pass the response to json dictionary and run the script passing the dict as an argument to iteration function
 # dump the output to file if arg was speficied
 
-if args.fileout: 
-    dumptofile(str(print_commit_attr(requests.get(url, headers=headers).json())), args.fileout)
-else: exit
+if cliargs.fileout: 
+    print_commit_attr((requests.get(url, headers=headers).json()), cliargs.fileout)
+else: print_commit_attr(requests.get(url, headers=headers).json())
